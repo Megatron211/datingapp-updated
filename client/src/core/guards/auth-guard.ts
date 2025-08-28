fa172ef26ab3eab40ae94c27af6e675f0f751b2a
givenvalentino@gmail.com <givenvalentino@gmail.com>
2025-08-28 17:23:59 +0200

    Error handling

--- client/src/core/guards/auth-guard.ts
+++ client/src/core/guards/auth-guard.ts
@@ -4,10 +4,10 @@ import { inject } from '@angular/core';
 import { ToastService } from '../services/toast-service';
 
 export const authGuard: CanActivateFn = () => {
- const accountServuce = inject(AccountService)
+ const accountService = inject(AccountService)
  const toast = inject(ToastService);
 
- if(accountServuce.currentUser()) return true;
+ if(accountService.currentUser()) return true;
  else{
   toast.error('You must be logged in to access this page');
   return false; 
---
+++ client/src/features/test-errors/test-errors.ts
@@ -0,0 +1,51 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, signal } from '@angular/core';
+
+@Component({
+  selector: 'app-test-errors',
+  imports: [],
+  templateUrl: './test-errors.html',
+  styleUrl: './test-errors.css'
+})
+export class TestErrors {
+  private http = inject(HttpClient);
+  baseUrl = "https://localhost:5001/api/";
+  validationErrors = signal<string[]>([]);
+
+  get404Error(){
+    this.http.get(this.baseUrl + "buggy/not-found").subscribe({
+      next: message => console.log(message),
+      error: error => console.log(error.error)
+    })
+  }
+  get400Error(){
+    this.http.get(this.baseUrl + "buggy/bad-request").subscribe({
+      next: message => console.log(message),
+      error: error => console.log(error.error)
+    })
+  }
+
+  get401Error(){
+    this.http.get(this.baseUrl + "buggy/auth").subscribe({
+      next: message => console.log(message),
+      error: error => console.log(error.error)
+    })
+  }
+
+  get500Error(){
+    this.http.get(this.baseUrl + "buggy/server-error").subscribe({
+      next: message => console.log(message),
+      error: error => console.log(error.error)
+    })
+  }
+
+  get400ValidationError(){
+    this.http.post(this.baseUrl + "account/register", {}).subscribe({
+      next: message => console.log(message),
+      error: error => {
+        console.log(error)
+        this.validationErrors.set(error);}
+    })
+  }
+
+}
